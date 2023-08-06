a2de95a646e52772c1c78b4a8cc98df8eedb46c1
otttisss <otttisss@yandex.ru>
2023-08-06 13:31:15 +0300

    RomanToInteger solved

--- RomanToInteger/RomanToInteger.java
+++ RomanToInteger/RomanToInteger.java
@@ -1,5 +1,52 @@
 package RomanToInteger;
 
-public class RomanToInteger {
+import java.util.HashMap;
+import java.util.Map;
 
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        Map<Character, Integer> romanNum = new HashMap<>();
+
+        romanNum.put('I', 1);
+        romanNum.put('V', 5);
+        romanNum.put('X', 10);
+        romanNum.put('L', 50);
+        romanNum.put('C', 100);
+        romanNum.put('D', 500);
+        romanNum.put('M', 1000);
+
+        int totalNum = 0;
+        int prev = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int value = romanNum.get(c);
+
+            if (value < prev) {
+                totalNum = totalNum - value;
+            } else {
+                totalNum = totalNum + value;
+            }
+            prev = value;
+        }
+
+        return totalNum;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "III";
+        String str2 = "IV";
+        String str3 = "LVIII";
+        String str4 = "MCMXCIV";
+
+        int total1 = romanToInt(str1);
+        int total2 = romanToInt(str2);
+        int total3 = romanToInt(str3);
+        int total4 = romanToInt(str4);
+
+        System.out.println(total1);
+        System.out.println(total2);
+        System.out.println(total3);
+        System.out.println(total4);
+    }
 }
