7112962b950a3735b5ed05adeb24d18abc39bcd6
otttisss <otttisss@yandex.ru>
2023-08-06 14:48:41 +0300

    LongesSubstring Medium LVL started

---
+++ LongestSubstringWithoutRepeatingChars/LongesSubstring.java
@@ -0,0 +1,13 @@
+package LongestSubstringWithoutRepeatingChars;
+
+public class LongesSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+
+    }
+
+    public static void main(String[] args) {
+        String s = "abcdefwwwewqqq";
+
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+}
