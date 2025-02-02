a3815e2e54599c6feb83e86650338ce4122d8f07
efimotis <efimotis@gmail.com>
2025-02-02 20:13:20 +0300

    MergeTwoLists

---
+++ MergeTwoLists/MergeTwoLists.java
@@ -0,0 +1,64 @@
+package MergeTwoLists;
+
+/**
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ * Return the head of the merged linked list.
+ */
+
+public class MergeTwoLists {
+
+    static class ListNode {
+
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        //
+        ListNode mergedList = mergeTwoLists(list1, list2);
+
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ----> ");
+            mergedList = mergedList.next;
+        }
+    }
+
+}
